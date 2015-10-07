746d84ece72a60548030c18c6078d3fe4d98b0b2
Antun Matanović <antun.matanovic@gmail.com>
2015-10-07 18:08:28 +0200

    commit

--- index.php
+++ index.php
@@ -1,69 +1,9 @@
 <?php include 'konfiguracija.php'; 
-session_start();    
+session_start();  
+include 'head.php';  
 ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Local boost</title>
 
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/styles.css" rel="stylesheet">
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-
-    <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li><a href="#home">HOME</a></li>
-            <li><a href="#onama">O NAMA</a></li>
-            <li><a href="#projekti">PROJEKTI</a></li>
-            <?php if(isset($_SESSION['autoriziran'])){
-      ?>
-      <li><a href="mojiprojekti.php"><li>MOJI PROJEKTI</li></a>
-   <?php } ?>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-                     <?php
-    if(isset($_SESSION['autoriziran'])){
-      ?>
-   <li><a href="logout.php"><li>ODJAVA</li></a>
-   <?php } else {?>
-            <li><a href="#prijava">PRIJAVA</a></li>
-            <li><a href="#registracija">REGISTRACIJA</a></li>
-              <?php }?>
-            <li><span class="glyphicon glyphicon-search"></span></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
-      
-
-    <div id="home">  
-      
-      <img src="slike/milino-jezero.jpg" alt="početna slika" class="pocetnaslika">
-
-
-      <div class="naslovi" id="onama">
+<div class="naslovi" id="onama">
 
 <h1>O nama</h1>
 <p class="tekst">Local Boost je stranica na kojoj možete financijski poduprijeti tuđe projekte, kao i drugi Vaše.</p>
--- mojiprojekti.php
+++ mojiprojekti.php
@@ -1,77 +1,32 @@
 <?php 
+include 'konfiguracija.php'; 
 session_start();
 if(!isset($_SESSION['autoriziran'])){
   header("location: ../odjava.php");  
 }
+include 'head.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Local boost</title>
-
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/styles.css" rel="stylesheet">
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-
-    <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li><a href="index.php#home">HOME</a></li>
-            <li><a href="index.php#onama">O NAMA</a></li>
-            <li><a href="index.php#projekti">PROJEKTI</a></li>
-            <?php if(isset($_SESSION['autoriziran'])){
-      ?>
-      <li><a href="mojiprojekti.php#mojiprojekti"><li>MOJI PROJEKTI</li></a>
-   <?php } ?>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-                     <?php
-    if(isset($_SESSION['autoriziran'])){
-      ?>
-   <li><a href="logout.php"><li>ODJAVA</li></a>
-   <?php } else {?>
-            <li><a href="index.php#prijava">PRIJAVA</a></li>
-            <li><a href="index.php#registracija">REGISTRACIJA</a></li>
-              <?php }?>
-            <li><span class="glyphicon glyphicon-search"></span></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
-      
-
-    <div id="home">  
-      
-      <img src="slike/milino-jezero.jpg" alt="početna slika" class="pocetnaslika">
-
-</div>
 
 <div class="container" id="mojiprojekti">
-
+<?php
+$izraz=$veza->prepare("select * from projekt");
+$izraz->execute();
+$projekti=$izraz->fetchAll(PDO::FETCH_OBJ);
+if ($projekti==null) {
+  echo "Nemate projekata";
+}
+else {
+foreach ($projekti as $projekt) {
+  echo "<p>" . $projekt->naziv . "</p>";
+  echo "<a class='button' href='promjena.php?sifra=" . $projekt->sifra . "' style='width: 100%'>Izmjeni</a>";
+}
+}
+?>
 
 </div>
-     
+<div class="container">
+<a class='button' href="noviprojekt.php" style='width: 100%'>Dodaj novi projekt</a>
+</div>   
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
