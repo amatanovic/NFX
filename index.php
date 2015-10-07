200de2d18ee788c85d172c1f0f414b7121713d83
Antun Matanović <antun.matanovic@gmail.com>
2015-10-07 17:05:48 +0200

    smooth scroll

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ session_start();
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
-            <li><a href="#">HOME</a></li>
+            <li><a href="#home">HOME</a></li>
             <li><a href="#onama">O NAMA</a></li>
             <li><a href="#projekti">PROJEKTI</a></li>
           </ul>
@@ -44,8 +44,8 @@ session_start();
       ?>
    <li><a href="logout.php"><li>ODJAVA</li></a>
    <?php } else {?>
-            <li><a href="../navbar/">PRIJAVA</a></li>
-            <li><a href="../navbar-static-top/">REGISTRACIJA</a></li>
+            <li><a href="#prijava">PRIJAVA</a></li>
+            <li><a href="#registracija">REGISTRACIJA</a></li>
               <?php }?>
             <li><span class="glyphicon glyphicon-search"></span></li>
           </ul>
@@ -54,12 +54,12 @@ session_start();
     </nav>
       
 
-      
+    <div id="home">  
       
       <img src="slike/milino-jezero.jpg" alt="početna slika" class="pocetnaslika">
 
-
-      <div class="naslov">
+    </div>
+      <div class="naslov" id="onama">
 <h1>O nama</h1>
 <p class="tekst">Local Boost je stranica na kojoj možete financijski poduprijeti tuđe projekte, kao i drugi Vaše.</p>
 <p class="tekst">Kako biste postavili vlastiti projekt, potrebno je obaviti registraciju.</p>
@@ -68,7 +68,7 @@ session_start();
 <p class="tekst">Svaki prijavljeni korisnik ima mogućnost objaviti vlastiti projekt koji omogućuje drugima da ga financiraju.</p>
 <p class="tekst">Local Boost Vam pomaže u realizaciji raznovrsnih projekata i potiče Vaše ideje i napredak.</p>
       </div>
-<div class="container">
+<div class="container" id="projekti">
 <?php
 $izraz=$veza->prepare("select * from projekt");
 $izraz->execute();
@@ -89,7 +89,8 @@ foreach ($projekti as $projekt) {
 }
 ?>
 </div>
-      
+    
+    <div id="prijava">  
     <?php
     if(!isset($_SESSION['autoriziran'])){ ?>
     <form action="#" id="login">
@@ -101,8 +102,9 @@ foreach ($projekti as $projekt) {
   </form>
   <p id="poruka"></p>
   <?php } ?>
+</div>      
       
-      
+  <div id="registracija">
    <?php
     if(!isset($_SESSION['autoriziran'])){ ?>
     <form action="#" id="registracija">
@@ -118,7 +120,7 @@ foreach ($projekti as $projekt) {
   </form>
   <p id="registracijaPoruka"></p>
   <?php } ?>
-
+</div>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
@@ -191,7 +193,23 @@ foreach ($projekti as $projekt) {
 
         return false;
       });
-        }); 
+        });
+        $(function () {
+        $('a[href*=#]:not([href=#])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                    || location.hostname == this.hostname) {
+
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 700);
+                    return false;
+                }
+            }
+        });
+    }); 
   </script> 
   </body>
 </html>
