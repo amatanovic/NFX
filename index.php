65237826e3f76b9de14d6f7ddfc630d89181cbc4
Antun Matanović <antun.matanovic@gmail.com>
2015-10-07 18:19:28 +0200

    commit

--- index.php
+++ index.php
@@ -3,60 +3,6 @@ session_start();
 include 'head.php';  
 ?>
 
-<div class="naslovi" id="onama">
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/styles.css" rel="stylesheet">
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-
-    <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li><a href="#home">HOME</a></li>
-            <li><a href="#onama">O NAMA</a></li>
-            <li><a href="#projekti">PROJEKTI</a></li>
-            <?php if(isset($_SESSION['autoriziran'])){
-      ?>
-      <li><a href="mojiprojekti.php"><li>MOJI PROJEKTI</li></a>
-   <?php } ?>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-                     <?php
-    if(isset($_SESSION['autoriziran'])){
-      ?>
-   <li><a href="logout.php"><li>ODJAVA</li></a>
-   <?php } else {?>
-            <li><a href="#prijava" data-toggle="modal">PRIJAVA</a></li>
-            <li><a href="#registracija">REGISTRACIJA</a></li>
-              <?php }?>
-            <li><span class="glyphicon glyphicon-search"></span></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
-      
-
-    <div id="home">  
-      
-      <img src="slike/milino-jezero.jpg" alt="početna slika" class="pocetnaslika">
-
-
       <div class="naslovi" id="onama">
 
 <h1>O nama</h1>
