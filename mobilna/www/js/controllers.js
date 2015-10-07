88d2a5158b4e4d1d7d976065ec5394c6dd000f72
tvilcek <vilcek.tena@gmail.com>
2015-10-07 17:09:37 +0200

    jaja

--- mobilna/www/js/controllers.js
+++ mobilna/www/js/controllers.js
@@ -1,28 +1,30 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('LogoutCtrl', function($scope) {
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('LoginCtrl', function($scope) {
+	$scope.login={
+	email: "sdbgjdsb",
+	password: "ldlgjsdg"
+};
+
+$scope.submit=function(){
+console.log($scope.login);
+
+};
+
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+
+.controller('ProjektiCtrl', function($scope) {
+
+
+})
+
+.controller('DetaljiCtrl', function($scope) {
+	
+
 });
