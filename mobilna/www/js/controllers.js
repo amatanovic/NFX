8e5847d31c17f4504dbee8e466e97d054bfc19d4
tvilcek <vilcek.tena@gmail.com>
2015-10-08 22:18:09 +0200

    neš

--- mobilna/www/js/controllers.js
+++ mobilna/www/js/controllers.js
@@ -7,11 +7,50 @@ $scope.logout = false;
 
 })
 
-.controller('LoginCtrl', function($scope) {
-	$scope.login={
-	email: "sdbgjdsb",
-	password: "ldlgjsdg"
-};
+.controller('LoginCtrl', function($scope, $rootScope, $ionicLoading, $http, $state) {
+	
+	$scope.alert = false;
+	$scope.upozorenje = false;
+	$rootScope.userData = {};
+	$scope.login  = {
+    	email: "",
+    	password: ""
+    };
+
+	$scope.submit = function (){
+	if ($scope.login.email === "" || $scope.login.password === "") {
+		$scope.alert = true;
+		}
+		else if ($scope.login.email !== "" || $scope.login.password !== "") {
+			$scope.alert = false;
+			$ionicLoading.show({
+					template: '<ion-spinner></ion-spinner><br />Please wait...'
+				});		
+				$http({
+					url: "http://localhost/knowledge/android/login.php",
+					data: $scope.login,
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/x-www-form-urlencoded'
+					}
+				}).success(function(data) {
+					$ionicLoading.hide();
+					if (data === false) {
+						$scope.incorrectData = true;
+					}
+					else if (data !== false) {
+						$rootScope.userData = data;
+						$rootScope.loggedIn = true;
+						$state.go("");
+					}
+					
+
+				}).error(function(err) {
+					$ionicLoading.hide();
+					$scope.alert = true;
+				});
+		}
+    };
 
 $scope.submit=function(){
 console.log($scope.login);
