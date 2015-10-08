da3d2b1a5f6a3eb57765ba70a06a26cd2e266ab4
Manuela <mmikulecki@gmail.com>
2015-10-09 01:41:07 +0200

    search

--- search-modal.php
+++ search-modal.php
@@ -7,17 +7,19 @@
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
         <h1 class="modal-title">Pretraživanje</h1>
-          <hr />
+          
       </div>
       <div class="modal-body">
           
-        <h2>Jednostavno pretraživanje</h2>
-          <hr />
-          <div>  <label for="tag">Ključne riječi:</label> <input type="text" id="tag" name="uvjet" placeholder="Unesite ključnu riječ" /> </div> 
+        <h2 class="pretrazi">JEDNOSTAVNO PRETRAŽIVANJE</h2>
           
-        <h2>Složeno pretraživanje</h2>
-          <hr />
-          <label for="kategorije">Kategorije:</label>
+        
+          <div class="kljucne">  <label for="tag">Ključne riječi:</label> <input type="text" id="tag" name="uvjet" placeholder="Unesite ključnu riječ" /> </div> 
+          <br />
+       
+        <h2 class="pretrazi">SLOŽENO PRETRAŽIVANJE</h2>
+          
+          <div class="kljucne"><label for="kategorije">Kategorije:</label>
           <?php 
           echo "<select id='uvjetKategorije' name='uvjetKategorije'><option value='' selected='selected'>Sve kategorije</option>";
 $izraz = $veza->prepare("select * from kategorija");
@@ -29,10 +31,11 @@ echo "<option value=\"" . $en->sifra . "\">" . $en->naziv ."</option>";
           }
 echo "</select>";
 ?>
+</div>
       </div>
       <div class="modal-footer">
-        <button type="button submit" class="btn btn-default" name="search" id="searchStart">SEARCH</button>
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button submit" class="btn btn-default" name="search" id="searchStart">PRETRAŽI</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Zatvori</button>
       </div>
     </div>
 
