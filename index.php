491132f8f76c0ca53aa19cc76768a2d3e21a1b2d
Ana Leh <ana.leh05@gmail.com>
2015-10-10 14:26:44 +0200

    g

--- index.php
+++ index.php
@@ -1,7 +1,23 @@
 <?php include 'head.php';  ?>
-      
-      
-      
+ 
+
+
+<div id="home">  
+<img class="naslovna-slika" src="slike/jabuke.jpg" alt="naslovna stranica jabuka   " />
+</div>
+
+
+      <div class="naslovi" id="onama">
+<h1>O nama</h1>
+          <br />
+<p class="tekst">Local Boost je stranica na kojoj možete financijski poduprijeti tuđe projekte, kao i drugi Vaše.</p>
+<p class="tekst">Kako biste postavili vlastiti projekt, potrebno je obaviti registraciju.</p>
+<p class="tekst">Nakon izrade korisničkog računa, možete se prijavljivati sa emailom i odabranom lozinkom.</p>
+<p class="tekst">Svaki prijavljeni korisnik ima mogućnost financiranja tuđih projekata jednostavnim klikom na uplatu.</p>
+<p class="tekst">Svaki prijavljeni korisnik ima mogućnost objaviti vlastiti projekt koji omogućuje drugima da ga financiraju.</p>
+<p class="tekst">Local Boost Vam pomaže u realizaciji raznovrsnih projekata i potiče Vaše ideje i napredak.</p>
+          
+      </div>
       
       
       
