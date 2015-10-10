fc0a7764982989e410e83d8877b7dbec437b8efc
Ana Leh <ana.leh05@gmail.com>
2015-10-10 19:18:03 +0200

    uređivanje navigacije responsive

--- head.php
+++ head.php
@@ -10,8 +10,8 @@
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/styles.css" rel="stylesheet">
-    <link href="css/styles-medium.css" rel="stylesheet">
-    <link href="css/styles-small.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" media="screen and (max-width: 1119px)" href="css/styles-medium.css" />
+    <link rel="stylesheet" type="text/css" media="screen and (max-width: 950px)" href="css/styles-small.css" />
     <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:400,300' rel='stylesheet' type='text/css'>
       <link rel="icon" href="slike/logo.ico" type="image/x-icon">
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
