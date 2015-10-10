12a3c67108909d5a0952aed77dac4121558757a4
Antun Matanović <antun.matanovic@gmail.com>
2015-10-10 17:12:48 +0200

    scroll

--- index.php
+++ index.php
@@ -126,6 +126,22 @@ foreach ($opgi as $opg) {
         return false;
       });
         });
+       $(function () {
+        $('a[href*=#]:not([href=#])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                    || location.hostname == this.hostname) {
+
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 700);
+                    return false;
+                }
+            }
+        });
+    }); 
     </script>
   </body>
 </html>
