48a93b3fd39c6e59c6928e524ecb193f317902cf
Ana Leh <ana.leh05@gmail.com>
2015-10-10 17:20:04 +0200

    search dodan

---
+++ search-modal.php
@@ -0,0 +1,29 @@
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h3 class="modal-title">Pretraživanje</h3>
+      </div>
+      <div class="modal-body">
+          
+          <div class="kategorije">  
+            <label for="proizvod">Proizvod:</label> 
+            <option></option>
+          </div> 
+
+          <div class="proizvod">  
+            <label for="proizvod">Proizvod:</label> 
+            <input type="text" id="tag" name="uvjet" placeholder="Unesite željeni  proizvod" /> 
+          </div> 
+        
+        </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
